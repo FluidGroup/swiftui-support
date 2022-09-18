b63370da0c7e35d4a5ad94893b208c0c117fcc2c
Muukii <muukii.app@gmail.com>
2022-09-19 01:48:49 +0900

    :evergreen_tree: Update

---
+++ Sources/SwiftUISupport/SwiftUISupport.swift
@@ -0,0 +1,7 @@
+
+/**
+ SwiftUISupport is a module that provides functions to help SwiftUI development.
+ We don't know which supports exactly we need yet, we will clean them up later.
+ */
+
+import SwiftUI
--- Sources/SwiftUISupport/View+.swift
+++ Sources/SwiftUISupport/View+.swift
@@ -1,24 +1,5 @@
 
-/**
- SwiftUISupport is a module that provides functions to help SwiftUI development.
- We don't know which supports exactly we need yet, we will clean them up later.
- */
-
 import SwiftUI
-
-extension ForEach where Data == Range<Int>, ID == Int, Content: View {
-  
-  /**
-   For just displaying particularly prototyping.
-   Not for suitable in production.
-   */
-  public static func inefficient<Item>(items: [Item], @ViewBuilder body: @escaping (Item) -> Content) -> ForEach {
-    ForEach(items.indices, id: \.self) { index in
-      body(items[index])
-    }
-  }
-  
-}
 
 extension View {
   

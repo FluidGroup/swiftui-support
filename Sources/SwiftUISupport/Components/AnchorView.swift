65afde9d40d08f0b79422c255727eb4367ded99b
Muukii <muukii.app@gmail.com>
2023-01-12 04:14:00 +0900

    :evergreen_tree: Update

---
+++ Sources/SwiftUISupport/Components/AnchorView.swift
@@ -0,0 +1,76 @@
+import SwiftUI
+
+public enum RelativeHorizontalPosition {
+  case left
+  case center
+  case right
+}
+
+public enum RelativeVerticalPosition {
+  case top
+  case center
+  case bottom
+}
+
+public struct RelativeView<Content: View>: View {
+
+  let content: Content
+  let vertical: RelativeVerticalPosition
+  let horizontal: RelativeHorizontalPosition
+
+  init(
+    vertical: RelativeVerticalPosition,
+    horizontal: RelativeHorizontalPosition,
+    @ViewBuilder content: () -> Content
+  ) {
+    self.vertical = vertical
+    self.horizontal = horizontal
+    self.content = content()
+  }
+
+  public var body: some View {
+
+    let horizontalContent: some View = HStack {
+      switch horizontal {
+      case .left:
+        content
+        Spacer()
+      case .center:
+        content
+      case .right:
+        Spacer()
+        content
+      }
+    }
+
+    VStack {
+      switch vertical {
+      case .top:
+        horizontalContent
+        Spacer()
+      case .center:
+        horizontalContent
+      case .bottom:
+        Spacer()
+        horizontalContent
+      }
+    }
+
+  }
+}
+
+enum Preview_AnchorView: PreviewProvider {
+
+  static var previews: some View {
+    Rectangle()
+      .frame(width: 50, height: 50)
+      .overlay(
+        RelativeView(vertical: .top, horizontal: .right) {
+          Circle()
+            .foregroundColor(.red)
+            .frame(width: 20, height: 20)
+        }
+        .offset(x: 10, y: -10)
+      )
+  }
+}

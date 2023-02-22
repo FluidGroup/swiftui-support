bd52ec2ca4a9e9787a222542ef9d7ffff76a6fd7
Muukii <muukii.app@gmail.com>
2023-02-22 23:11:21 +0900

    Update gradient

--- Sources/SwiftUISupport/Extensions/View+Gradient.swift
+++ Sources/SwiftUISupport/Extensions/View+Gradient.swift
@@ -1,13 +1,18 @@
 import SwiftUI
 
 extension Text {
-  
+
   /**
    [Extension]
    */
   public func foregroundLinearGradient(_ gradient: LinearGradient) -> some View {
-    self.overlay(gradient).mask(self)
-  }
-  
-}
 
+    self
+      .overlay(
+        gradient
+          .aspectRatio(nil, contentMode: .fill)
+      )
+      .mask(self)
+  }
+
+}

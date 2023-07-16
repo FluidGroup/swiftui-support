e5174d0e95b13bcf668db285c11201e2d01ac041
Muukii <muukii.app@gmail.com>
2023-07-16 12:27:40 +0900

    Add Async

--- Sources/SwiftUISupport/Components/ObservedView.swift
+++ Sources/SwiftUISupport/Components/ObservedView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import Combine
 
 public struct ObservedView<Object: ObservableObject, Content: View>: View {
 
@@ -14,4 +15,56 @@ public struct ObservedView<Object: ObservableObject, Content: View>: View {
     content(object)
   }
 
+}
+
+public struct AsyncObservedView<Object: ObservableObject, Content: View>: View {
+
+  public struct _ObservedView: View {
+
+    @ObservedObject var object: Object
+    private let content: (Object) -> Content
+
+    public init(object: Object, @ViewBuilder content: @escaping (Object) -> Content) {
+      self.content = content
+      self._object = .init(wrappedValue: object)
+    }
+
+    public var body: some View {
+      content(object)
+    }
+
+  }
+
+  @State var loaded: Object?
+
+  private let content: (Object) -> Content
+  private let objectLoader: () async -> Object
+
+  public init(
+    objectLoader: @escaping @Sendable () async -> Object,
+    @ViewBuilder content: @escaping (Object) -> Content
+  ) {
+    self.content = content
+    self.objectLoader = objectLoader
+  }
+
+  public var body: some View {
+
+    Group {
+      if let loaded {
+        _ObservedView(object: loaded, content: content)
+      }
+    }
+    .onAppear {
+      Task.detached {
+        let loaded = await objectLoader()
+
+        await MainActor.run {
+          self.loaded = loaded
+        }
+
+      }
+    }
+
+  }
 }

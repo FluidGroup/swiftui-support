9a08b1411a97825f67ccd4f4913bd7364a532a64
Muukii <muukii.app@gmail.com>
2023-11-22 21:56:02 +0900

    Patch

--- Sources/SwiftUISupport/Components/StyleModifier.swift
+++ Sources/SwiftUISupport/Components/StyleModifier.swift
@@ -7,17 +7,20 @@ public struct StyleModifier: ViewModifier {
   public let scale: CGSize
   public let offset: CGSize
   public let blurRadius: Double
-  
+  public let rotationEffect: Angle
+
   public init(
     opacity: Double = 1,
     scale: CGSize = .init(width: 1, height: 1),
     offset: CGSize = .zero,
-    blurRadius: Double = 0
+    blurRadius: Double = 0,
+    rotationEffect: Angle = .zero
   ) {
     self.opacity = opacity
     self.scale = scale
     self.offset = offset
     self.blurRadius = blurRadius
+    self.rotationEffect = rotationEffect
   }
   
   public func body(content: Content) -> some View {
@@ -27,6 +30,7 @@ public struct StyleModifier: ViewModifier {
       .opacity(opacity)
       .scaleEffect(scale)
       .blur(radius: blurRadius)
+      .rotationEffect(rotationEffect)
   }
 
   public static var identity: Self {

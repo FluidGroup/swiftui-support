37c48a3d8ca92109aa8a5a8b476e247039dc2a54
Hiroshi Kimura <muukii.app@gmail.com>
2024-07-13 14:30:55 +0900

    map (#40)

--- Sources/SwiftUISupport/Components/Conditional.swift
+++ Sources/SwiftUISupport/Components/Conditional.swift
@@ -1,13 +1,17 @@
 import SwiftUI
 
 extension View {
+
   /// Applies the given transform if the given condition evaluates to `true`.
   /// @see Referenced from: https://www.avanderlee.com/swiftui/conditional-view-modifier/
   /// - Parameters:
   ///   - condition: The condition to evaluate.
   ///   - transform: The transform to apply to the source `View`.
   /// - Returns: Either the original `View` or the modified `View` if the condition is `true`.
-  @ViewBuilder public func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
+  @ViewBuilder public consuming func `if`<Content: View>(
+    _ condition: Bool,
+    transform: (Self) -> Content
+  ) -> some View {
     if condition {
       transform(self)
     } else {
@@ -16,9 +20,29 @@ extension View {
   }
 
   @ViewBuilder
-  public func `do`<Content: View>(
+  public consuming func `do`<Content: View>(
     @ViewBuilder transform: (Self) -> Content
   ) -> some View {
     transform(self)
   }
+
+  /**
+   ```
+   Text("...")
+     .map { view in
+        if condition {
+          return view
+        } else {
+          return view
+            .padding()
+        }
+     }
+   ```
+   */
+  public consuming func map<Content: View>(
+    @ViewBuilder transform: (consuming Self) -> Content
+  ) -> some View {
+    transform(self)
+  }
+
 }

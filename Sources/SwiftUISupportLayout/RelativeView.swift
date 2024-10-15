4d5feb387099c4c0f719fcc5b49546d862cc4954
Muukii <muukii.app@gmail.com>
2024-10-15 23:53:43 +0900

    Patch

--- Sources/SwiftUISupportLayout/RelativeView.swift
+++ Sources/SwiftUISupportLayout/RelativeView.swift
@@ -1,67 +1,5 @@
 import SwiftUI
 
-public enum RelativeHorizontalPosition {
-  case left
-  case center
-  case right
-}
-
-public enum RelativeVerticalPosition {
-  case top
-  case center
-  case bottom
-}
-
-/// A container view that places the content in the specified position.
-/// A useful case will be placing the content on the corner of the parent view.
-@available(*, deprecated, message: "Use modifier instead")
-public struct RelativeView<Content: View>: View {
-
-  public let content: Content
-  public let vertical: RelativeVerticalPosition
-  public let horizontal: RelativeHorizontalPosition
-
-  public init(
-    vertical: RelativeVerticalPosition,
-    horizontal: RelativeHorizontalPosition,
-    @ViewBuilder content: () -> Content
-  ) {
-    self.vertical = vertical
-    self.horizontal = horizontal
-    self.content = content()
-  }
-
-  public var body: some View {
-
-    let horizontalContent: some View = HStack {
-      switch horizontal {
-      case .left:
-        content
-        Spacer(minLength: 0)
-      case .center:
-        content
-      case .right:
-        Spacer(minLength: 0)
-        content
-      }
-    }
-
-    VStack {
-      switch vertical {
-      case .top:
-        horizontalContent
-        Spacer(minLength: 0)
-      case .center:
-        horizontalContent
-      case .bottom:
-        Spacer(minLength: 0)
-        horizontalContent
-      }
-    }
-
-  }
-}
-
 /// like align-self
 public struct RelativeLayoutModifier: ViewModifier {
 
@@ -133,37 +71,49 @@ extension View {
 
 }
 
-enum Preview_AnchorView: PreviewProvider {
-
-  static var previews: some View {
-    Rectangle()
-      .frame(width: 50, height: 50)
-      .overlay(
-        RelativeView(vertical: .top, horizontal: .right) {
-          Circle()
-            .foregroundColor(.red)
-            .frame(width: 20, height: 20)
-        }
+#Preview {
+  Rectangle()
+    .frame(width: 50, height: 50)
+    .overlay(
+      Circle()
+        .foregroundColor(.red)
+        .frame(width: 20, height: 20)
+        .relative(vertical: .top, horizontal: .trailing)
         .offset(x: 10, y: -10)
-      )
-
-    Rectangle()
-      .frame(width: 50, height: 50)
-      .overlay(
-        Circle()
-          .foregroundColor(.red)
-          .frame(width: 20, height: 20)
-          .relative(vertical: .top, horizontal: .trailing)
-          .offset(x: 10, y: -10)
-      )
-
-    HStack {
-      Text("A")
-      Text("B")
-      Text("C")
-    }
-    .environment(\.layoutDirection, .rightToLeft)
-
+    )
+  
+  Rectangle()
+    .frame(width: 50, height: 50)
+    .overlay(
+      Circle()
+        .foregroundColor(.red)
+        .frame(width: 20, height: 20)
+        .relative(vertical: .top, horizontal: .trailing)
+        .offset(x: 10, y: -10)
+    )
+  
+  HStack {
+    Text("A")
+    Text("B")
+    Text("C")
   }
+  .environment(\.layoutDirection, .rightToLeft)
+}
 
+#Preview("Using Aligment guide") {
+  
+  Rectangle()
+    .frame(width: 50, height: 50)
+    .overlay(alignment: .topTrailing) {
+      Circle()
+        .fill(.blue)
+        .frame(width: 20, height: 20)
+        .alignmentGuide(.top) { d in
+          return d[.top] + d.height / 2
+        }
+        .alignmentGuide(.trailing) { d in
+          return d[.trailing] - d.width / 2
+        }
+    }
+    .background(Color.purple)
 }

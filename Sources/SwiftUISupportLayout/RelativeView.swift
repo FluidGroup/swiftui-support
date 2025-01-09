f218bdafe6685642f1500bd168b86f7f87a1d70c
Muukii <muukii.app@gmail.com>
2025-01-09 12:54:42 +0900

    Patch

--- Sources/SwiftUISupportLayout/RelativeView.swift
+++ Sources/SwiftUISupportLayout/RelativeView.swift
@@ -1,98 +1,11 @@
 import SwiftUI
 
-/// deprecated
-public struct RelativeLayoutModifier {
-
-  public enum HorizontalPosition {
-    case leading
-    @available(*, unavailable)
-    case center
-    case trailing
-  }
-
-  public enum VerticalPosition {
-    case top
-    @available(*, unavailable)
-    case center
-    case bottom
-  }
-
-}
-
 extension View {
 
   /**
-   * Lays out the view and positions it within the layout bounds according to vertical and horizontal positional specifiers.
-   *  Can position the child at any of the 4 corners, or the middle of any of the 4 edges, as well as the center - similar to "9-part" image areas.
+   Using frame modifier inside, let the view place itself relative to the parent view.
    */
-  @available(*, deprecated, message: "use relative(alignment: )")
-  public func relative(
-    vertical: RelativeLayoutModifier.VerticalPosition,
-    horizontal: RelativeLayoutModifier.HorizontalPosition
-  ) -> some View {
-            
-    return self.relative(alignment: .init(
-      horizontal: {
-        switch horizontal {
-        case .center:
-          return .center
-        case .leading:
-          return .leading
-        case .trailing:
-          return .trailing
-        }
-      }(),
-      vertical: {
-        switch vertical {
-        case .center:
-          return .center
-        case .top:
-          return .top
-        case .bottom:
-          return .bottom
-        }        
-      }()
-    )
-    )    
-  }
-  
-  @available(*, deprecated, message: "use relative(alignment: )")
-  public func relative(
-    horizontal: RelativeLayoutModifier.HorizontalPosition
-  ) -> some View {
-    
-    return self.relative(horizontalAlignment: {
-      switch horizontal {
-      case .center:
-        return .center
-      case .leading:
-        return .leading
-      case .trailing:
-        return .trailing
-      }
-    }()
-    )    
-  }
-  
-  @available(*, deprecated, message: "use relative(alignment: )")
-  public func relative(
-    vertical: RelativeLayoutModifier.VerticalPosition
-  ) -> some View {
-    
-    return self.relative(verticalAlignment: {
-      switch vertical {
-      case .center:
-        return .center
-      case .top:
-        return .top
-      case .bottom:
-        return .bottom
-      }        
-    }()                         
-    )    
-  }
-  
-  public func relative(
+  public nonisolated func relative(
     alignment: Alignment
   ) -> some View {           
     self.frame(
@@ -101,20 +14,29 @@ extension View {
       alignment: alignment
     )
   }
-  
-  public func relative(
+
+  /**
+   Using frame modifier inside, let the view place itself relative to the parent view.
+   */
+  public nonisolated func relative(
     horizontalAlignment: HorizontalAlignment
   ) -> some View {           
     self.frame(maxWidth: .infinity, alignment: .init(horizontal: horizontalAlignment, vertical: .center))
   }
-  
-  public func relative(
+
+  /**
+   Using frame modifier inside, let the view place itself relative to the parent view.
+   */
+  public nonisolated func relative(
     verticalAlignment: VerticalAlignment
   ) -> some View {    
     self.frame(maxHeight: .infinity, alignment: .init(horizontal: .center, vertical: verticalAlignment))
   }
-  
-  public func relative(
+
+  /**
+   Using frame modifier inside, let the view place itself relative to the parent view.
+   */
+  public nonisolated func relative(
     horizontalAlignment: HorizontalAlignment,
     verticalAlignment: VerticalAlignment
   ) -> some View {    
